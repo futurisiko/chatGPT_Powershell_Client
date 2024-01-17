1faf8389b8a8f471ee13bdd58906f5bc2357997d
futurisiko <118108000+futurisiko@users.noreply.github.com>
2024-01-17 15:21:49 +0100

    Update chatGPT.ps1
    Adapted to new models and changes into APIs

--- chatGPT.ps1
+++ chatGPT.ps1
@@ -6,23 +6,24 @@
 #   *** REMEMBER TO ADD YOUR API KEY ***
 #   -> https://beta.openai.com/account/api-keys
 #
+# chatGPT.ps1 use 3 parameters: model, max_token and temperature.
 #
-# chatGPT use 2 parameters: max_token and temperature.
+# -model = 	select which model to use.
+#			If not specified default value is 'gpt-3.5-turbo' .
+#			You can try 'gpt-4' to ask more complex question/tasks.
 #
 # -maxtokens = the maximum number of tokens to generate in completion.
 #              Default = 4000
-#              1000 tokens =~ 750 words
 #              Your prompt + max_tokens can't exceed the context lenght of the model.
 #
 # -temperature = defines the type of processing applied to answer.
-#                Default = 0.7
+#                Default = 0.5
 #
 #                - lower (e.g. 0.0 / 1.0):
 #                chatGPT will choose words with a higher probability of occurrence.
 #                Useful when we want to complete something with the most probable value/phrase/word.
 #                - higher (e.g. 1.0 / max 2.0):
-#                Very creative but inconsistent answers.
-#                Above 1.5 very inconsistent.
+#                Very "creative" but inconsistent answers.
 #
 # ---------------------------
 # --- Credits: futurisiko ---
@@ -34,8 +35,9 @@
 param(
     [Parameter()][string]$questionparam,
     [Parameter()][switch]$helpparam,
-    [Parameter()]$temperatureparam = 0.7,
-    [Parameter()]$maxtokensparam = 4000
+    [Parameter()]$temperatureparam = 0.5,
+    [Parameter()]$maxtokensparam = 4000,
+	[Parameter()]$model = "gpt-3.5-turbo"
 )
 
 
@@ -64,10 +66,16 @@ function Help {
   Write-Host "`nchatGPT TERMINAL CLIENT" -ForegroundColor red
   Write-Host "`nSimple powershell script to query one direct question to chatGPT."
   Write-Host "`nUsage:" -NoNewline -ForegroundColor green
-  Write-Host " chatGTP.ps1 'Question in single quotes' [Options]"
+  Write-Host " .\chatGTP.ps1 -model <MODEL> -temperature <NUMBER> -maxtokens <NUMBER> 'Question in single quotes' [Options]"
+  Write-Host "`nExample:" -NoNewline -ForegroundColor yellow
+  Write-Host " .\chatGPT.ps1 'Write a powershell oneliner to launch a command to a servers list'"
   Write-Host "Example:" -NoNewline -ForegroundColor yellow
-  Write-Host " chatGPT.ps1 'Write a oneliner in powershell to get a reverse shell'"
+  Write-Host " .\chatGPT.ps1 -model gpt-4 -temperature 1.0 'Write a powershell oneliner to launch a command to a servers list'"
   Write-Host "`nOptions:"
+  Write-Host "`n-model <model_name>:" -ForegroundColor green -NoNewLine
+  Write-Host " select which model to use."
+  Write-Host "     If not specified default value is 'gpt-3.5-turbo' ."
+  Write-Host "     You can try 'gpt-4' to ask more complex question/tasks."
   Write-Host "`n-temperature <NUMBER>:" -ForegroundColor green -NoNewLine
   Write-Host " set the temperature used."
   Write-Host "     If not specified default value is 0.7."
@@ -80,7 +88,6 @@ function Help {
   Write-Host "`n-maxtokens <NUMBER>:" -ForegroundColor green -NoNewLine
   Write-Host " the maximum number of tokens to generate in completion."
   Write-Host "     If not specified default value is 4000."
-  Write-Host "     1000 tokens = 750 words."
   Write-Host "     Your prompt + max_tokens can't exceed the context lenght of the model."
   Write-Host "`n-help:" -ForegroundColor green -NoNewLine
   Write-Host " print this help."
@@ -106,6 +113,7 @@ if ( $PSBoundParameters.Values.Count -eq 0 -and $args.count -eq 0 ){
 ### Input feedback
 Write-Host "`n`n--------------------"
 Write-Host "`nQuestion: $questionparam" -ForegroundColor red
+Write-Host "Model: $model" -ForegroundColor red
 Write-Host "Temperature: $temperatureparam" -ForegroundColor red
 Write-Host "Max tokens: $maxtokensparam" -ForegroundColor red
 
@@ -115,33 +123,38 @@ Write-Host "`n--------------------"
 Write-Host "`nAnswer:" -ForegroundColor green
 
 
-### Other variables
-$model = "gpt-3.5-turbo"
+### Headers
+$headers = @{
+    "Content-Type" = "application/json"
+    "Authorization" = "Bearer $TOKEN"
+}
 
 
 ### Build the query (JSON)
-$payload = @{
+$payload = [ordered]@{
     model = $model
-    prompt = $questionparam
-    max_tokens = $maxtokensparam
+	messages = @(
+        [ordered]@{
+            role = "system"
+            content = $questionparam
+		}
+    )
+	max_tokens = $maxtokensparam
     temperature = $temperatureparam
 }
 $json = $payload | ConvertTo-Json
 
 
 ### Perform the query
-$response = Invoke-RestMethod -Uri 'https://api.openai.com/v1/chat/completions' -Method POST -Body $json -Headers @{Authorization = "Bearer $TOKEN"} -ContentType 'application/json'
+$response = Invoke-WebRequest -Uri "https://api.openai.com/v1/chat/completions" -Method Post -Headers $headers -Body $json
 
 
-### Extract the response
-$result = $response.choices | Select-Object -ExpandProperty text
+### Extract the responsees | Select-Object -ExpandProperty message | Select-Object -ExpandProperty content
+$result = $result = ($response.Content | ConvertFrom-Json).choices[0].message.content
 
 
 ### Output the result
 Write-Host $result -ForegroundColor green
 
-
 ### End
 Write-Host "`n--------------------`n`n"
-
-
